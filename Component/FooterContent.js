31117f4f868f4b12dd031099ef0f91788810f4f3
jooprima <jooprima@gmail.com>
2020-01-15 13:36:06 +0700

    footer

--- Component/FooterContent.js
+++ Component/FooterContent.js
@@ -5,22 +5,22 @@ import {Footer, Button, Icon, FooterTab} from 'native-base';
 export default class FooterContent extends Component {
   render() {
     return (
-      <Footer >
+      <Footer>
         <FooterTab style={{backgroundColor: 'white'}}>
           <Button>
-            <Icon style={{color:'black'}} active name="home" />
+            <Icon style={{color: 'black'}} active name="home" />
           </Button>
           <Button>
-            <Icon name="search" style={{color:'black'}}/>
+            <Icon name="search" style={{color: 'black'}} />
           </Button>
           <Button>
-            <Icon name="add" style={{color:'black'}}/>
+            <Icon name="add" style={{color: 'black'}} />
           </Button>
           <Button>
-            <Icon name="heart" style={{color:'black'}}/>
+            <Icon name="heart" style={{color: 'black'}} />
           </Button>
           <Button>
-            <Icon name="person" style={{color:'black'}}/>
+            <Icon name="person" style={{color: 'black'}} />
           </Button>
         </FooterTab>
       </Footer>
